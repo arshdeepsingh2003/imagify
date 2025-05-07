282acfc4b346d0c5c6647829c69fd3389883553c
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2025-05-07 22:10:41 +0530

    made navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
+import AppContextProvider from './context/AppContext';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <AppContextProvider>
+      <App/>
+    </AppContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
